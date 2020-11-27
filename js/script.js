d2f38c8e430482b47e3ae6dfbdf2ba0f0b8db0e2
Muhammad Azeem <37941410+AzeemSup@users.noreply.github.com>
2020-11-27 23:58:37 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("my-form");
+
+form.addEventListener('submit', function(e){
+	e.preventDefault();
+
+	const search = document.getElementById("search-area").value;
+
+	document.getElementById("result").innerHTML = "";
+
+	fetch("https://api.github.com/users/"+search)
+	.then((result) => result.json())
+	.then((data) => {
+		console.log(data);
+		document.getElementById("username").innerHTML = `
+			<div>Username: ${data.login}</div>
+		`;
+		document.getElementById("fullname").innerHTML = `
+			<div>Full Name: ${data.name}</div>
+		`;
+		document.getElementById("result").innerHTML = `
+			<a target="_blank" href="https://www.github.com/${search}">	<img alt="no user exist!!" src="${data.avatar_url}"/> </a>
+		`;
+	})
+})
